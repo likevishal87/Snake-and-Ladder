a1b9b9e6be4f67685b8dbf84f4393e639d119f4a
likevishal87 <likevishalverma@gmail.com>
2022-08-21 14:12:36 +0530

    UC1

---
+++ Startposition.java
@@ -0,0 +1,14 @@
+
+public class Startposition {
+
+	public static void main(String[] args) {
+		final int POSITION = 0;
+		
+		
+			System.out.println("Let's begin the game...");
+			System.out.println("Initial position is Square Zero");
+			System.out.println("Position : Square " + POSITION);
+
+	}
+
+}
